aa083f673a175fc49fc9d7772e7f860e7b13ca93
Felix <felix@hinterstellar.io>
2023-03-16 23:29:00 +0100

    [cml] Hamming tweak

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -6,7 +6,7 @@ let rec
     and l2 = List.length ns2 in
 
     match (l1, l2) with
-    | (l1, l2) when l1 = l2 -> Ok(List.fold_left2 (fun a n1 n2 -> check_pair a n1 n2) 0 ns1 ns2)
+    | (x, y) when x = y -> List.fold_left2 (fun a n1 n2 -> check_pair a n1 n2) 0 ns1 ns2 |> Result.ok
     | (0, _) -> Error("left strand must not be empty")
     | (_, 0) -> Error("right strand must not be empty")
     | _ -> Error("left and right strands must be of equal length")
