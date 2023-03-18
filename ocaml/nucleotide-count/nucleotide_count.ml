f601b54c7c154860f6e4a31e5b1fa11365e68c9e
Felix <felix@hinterstellar.io>
2023-03-18 14:49:42 +0100

    [cml] ocamlformat Nucleotide Count

--- ocaml/nucleotide-count/nucleotide_count.ml
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -6,22 +6,27 @@ let has_char (lst : char list) (c : char) : bool =
   List.exists lst ~f:(fun elt -> Char.equal c elt)
 
 let check_nucleotides (chars : char list) : (unit, char) Result.t =
-  let ok = ['A';'C';'G';'T'] in
+  let ok = ['A'; 'C'; 'G'; 'T'] in
   match List.find chars ~f:(fun c -> not (has_char ok c)) with
-  | Some(c) -> Error(c)
-  | _ -> Ok(())
+  | Some c ->
+      Error c
+  | _ ->
+      Ok ()
 
 let count_nucleotide s c =
-  match (Result.all[check_nucleotides (String.to_list s); check_nucleotides [c]]) with
-  | Error(bad) -> Error(bad)
-  | _ -> Ok(String.fold s ~init:0 ~f:(fun acc c2 -> if Char.equal c c2 then acc + 1 else acc))
+  match
+    Result.all [check_nucleotides (String.to_list s); check_nucleotides [c]]
+  with
+  | Error bad ->
+      Error bad
+  | _ ->
+      Ok
+        (String.fold s ~init:0 ~f:(fun acc c2 ->
+             if Char.equal c c2 then acc + 1 else acc ) )
 
 let count_nucleotides s =
-  let init = Ok(empty) in
-  String.fold s
-              ~init:init
-              ~f:(fun acc chr ->
-                Result.combine acc
-                               (count_nucleotide s chr)
-                               ~ok:(fun old count -> Map.set old ~key:chr ~data:count)
-                               ~err:(fun e1 e2 -> e1)) 
+  let init = Ok empty in
+  String.fold s ~init ~f:(fun acc chr ->
+      Result.combine acc (count_nucleotide s chr)
+        ~ok:(fun old count -> Map.set old ~key:chr ~data:count)
+        ~err:(fun e1 e2 -> e1) )
