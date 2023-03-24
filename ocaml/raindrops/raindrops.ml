919ab2cc5a46719bc9ee413e3b7924bc88a9796f
Felix <felix@hinterstellar.io>
2023-03-24 19:30:05 +0100

    [cml] Raindrops improved

--- ocaml/raindrops/raindrops.ml
+++ ocaml/raindrops/raindrops.ml
@@ -1,9 +1,8 @@
-let sound n factor word = if n mod factor = 0 then word else ""
-
 let raindrop n =
   let sounds =
-    [(3, "Pling"); (5, "Plang"); (7, "Plong")]
-    |> List.map (fun (factor, word) -> sound n factor word)
+    [ (3, "Pling"); (5, "Plang"); (7, "Plong") ]
+    |> List.filter (fun (fact, _) -> n mod fact = 0)
+    |> List.map snd
     |> List.fold_left ( ^ ) ""
   in
   if String.length sounds > 0 then sounds else Int.to_string n
