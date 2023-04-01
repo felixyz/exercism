aed9d06eb76fbe73d383e9a5f8d0b88b0aaffa4f
Felix <felix@hinterstellar.io>
2023-04-01 20:17:29 +0200

    [cml,flake] Add core_kernel + shell hook for opam

--- ocaml/flake.nix
+++ ocaml/flake.nix
@@ -13,6 +13,7 @@
           ocaml
           ocamlPackages.base
           ocamlPackages.findlib
+          ocamlPackages.core_kernel
           ocamlPackages.ocaml
           ocamlPackages.ounit2
           ocamlPackages.utop
@@ -24,6 +25,10 @@
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = myDevTools;
+          shellHook = ''
+            eval $(opam env --switch=ocaml-base-compiler)
+            echo $(ocaml --version)
+          '';
         };
       });
     }
