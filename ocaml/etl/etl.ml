ade1a50816b30709ff16459d62f33d445ec96fe3
Felix <felix@hinterstellar.io>
2023-04-11 15:19:39 +0200

    [cml] ETL - tail-recursive

--- ocaml/etl/etl.ml
+++ ocaml/etl/etl.ml
@@ -2,6 +2,7 @@ let transform old =
   old
   |> List.fold_left
        (fun a (points, letters) ->
-         a @ List.map (fun chr -> (Char.lowercase_ascii chr, points)) letters )
+         List.rev_map (fun chr -> (Char.lowercase_ascii chr, points)) letters
+         |> List.rev_append a )
        []
   |> List.sort (fun (c1, _) (c2, _) -> Char.compare c1 c2)
