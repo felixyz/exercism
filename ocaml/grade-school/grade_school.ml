ecd3c090116052f69bcc14fcb18e0c5ead0829bb
Felix <felix@hinterstellar.io>
2023-04-11 15:31:00 +0200

    [cml] Grade School

---
+++ ocaml/grade-school/grade_school.ml
@@ -0,0 +1,16 @@
+open Base
+module Int_map = Map.M (Int)
+
+type school = string list Int_map.t
+
+let empty_school = Map.empty (module Int)
+
+let add name grade school = Map.add_multi school ~key:grade ~data:name
+
+let grade grade school = Map.find_multi school grade
+
+let sorted school = school |> Map.map ~f:(List.sort ~compare:String.compare)
+
+let roster school =
+  school |> sorted |> Map.to_sequence |> Sequence.to_list
+  |> List.concat_map ~f:snd
