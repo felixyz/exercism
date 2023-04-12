6ff443376af941618104780fbaf09fadb2c41fb7
Felix <felix@hinterstellar.io>
2023-04-12 09:15:16 +0200

    [cml] Pangram, use Seq to succeed early

--- ocaml/pangram/pangram.ml
+++ ocaml/pangram/pangram.ml
@@ -1,14 +1,19 @@
 let is_pangram str =
-  let set b = ( lor ) (1 lsl b) in
-  str
-  |> String.fold_left
-       (fun a chr ->
-         match chr with
-         | 'A' .. 'Z' ->
-             set (Char.code chr - 65) a
-         | 'a' .. 'z' ->
-             set (Char.code chr - 97) a
-         | _ ->
-             a )
-       0
-  |> ( = ) 0b11111111111111111111111111
+  let set bit = ( lor ) (1 lsl bit)
+  and all_found = ( = ) 0b11111111111111111111111111 in
+  let rec aux bits seq =
+    if all_found bits then true
+    else
+      match Seq.uncons seq with
+      | None ->
+          false
+      | Some (chr, tl) -> (
+        match chr with
+        | 'A' .. 'Z' ->
+            aux (set (Char.code chr - 65) bits) tl
+        | 'a' .. 'z' ->
+            aux (set (Char.code chr - 97) bits) tl
+        | _ ->
+            aux bits tl )
+  in
+  String.to_seq str |> aux 0
